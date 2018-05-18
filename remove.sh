269d2013d54cc4f0aad705b72de1752192c0223f
Daniel Hand <email@danielhand.io>
2018-05-18 22:55:59 +0100

    Added api_url variable

--- remove.sh
+++ remove.sh
@@ -7,11 +7,12 @@
 
 # Vars
 subnet_id=9
+api_url=https://ipam.handsoff.cloud/api/vcenterapi
 green=`tput setaf 2`
 reset=`tput sgr0`
 
-token=$(curl -s -k -X POST --user $ipam_creds https://ipam.handsoff.cloud/api/vcenterapi/user | python -c 'import sys, json; print json.load(sys.stdin)["data"]["token"]')
+token=$(curl -s -k -X POST --user $ipam_creds $api_url/user | python -c 'import sys, json; print json.load(sys.stdin)["data"]["token"]')
 while read ip; do
-curl -s -k -X DELETE -H "token: $token" "https://ipam.handsoff.cloud/api/vcenterapi/addresses/$ip/$subnet_id"
+curl -s -k -X DELETE -H "token: $token" "$api_url/addresses/$ip/$subnet_id"
 echo ""
 done <ips.txt
