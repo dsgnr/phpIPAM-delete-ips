0651e0b0d329fb4757ad0ed647524d30d28cb4ff
Daniel Hand <email@danielhand.io>
2018-05-18 21:30:27 +0100

    Initial commit

---
+++ remove.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# @author: Daniel Hand
+# https://danielhand.io
+
+# Include secrets
+. ./secrets.sh
+
+# Vars
+subnet_id=7
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+token=$(curl -s -k -X POST --user $ipam_creds https://ipam.handsoff.cloud/api/vcenterapi/user | python -c 'import sys, json; print json.load(sys.stdin)["data"]["token"]')
+while read ip; do
+curl -s -k -X DELETE -H "token: $token" "https://ipam.handsoff.cloud/api/vcenterapi/addresses/$ip/$subnet_id"
+echo ""
+done <ips.txt
